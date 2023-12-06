09529859e94e9f7ff4d96e23018fab92abfabc0e
egorkudiarov <egerkud@gmail.com>
2023-12-06 15:49:09 +0400

    add solution to 1st task

--- 1, 2/1, 2.py
+++ 1, 2/1, 2.py
@@ -1,2 +1,18 @@
+def get_recipe(f, cook_book, name):
+    cook_book[name] = []
+    ingredients_amount = int(f.readline().strip())
+    for i in range(ingredients_amount):
+        ingredient = f.readline().strip().split(' | ')
+        cook_book[name].append({'ingredient_name': ingredient[0], 
+                                'quantity': ingredient[1], 
+                                'measure': ingredient[2]})
+    gap = f.readline()
 
-    
+def get_recipes():
+    cook_book = {}
+    with open('1/recipes.txt', 'r', encoding='UTF-8') as f:
+        while True:
+            name = f.readline().strip()
+            if name == '':
+                return cook_book
+            get_recipe(f, cook_book, name)
